6bd019fff770f8998c312bebdd6cf838ef7bcd54
Igor Kanel <ikanel@aligntech.com>
2025-06-05 20:53:34 -0400

    conectionsetting moved to separate file

--- pttMulticast/connectionSettings.py
+++ pttMulticast/connectionSettings.py
@@ -1,6 +1,6 @@
 MCAST_GRP = '224.0.0.251'
 MCAST_PORT = 5001
-IFACE = 'en0'  # Change if you’re using a different interface
-#IFACE = 'wlan0' #This configuration for the RaspberrycPI
+#IFACE = 'en0'  # Change if you’re using a different interface
+IFACE = 'wlan0' #This configuration for the RaspberrycPI
 CHANNEL=1 #1-25 for ptt 26-50 for paging
 CALLER="IGOR G KANEL"
